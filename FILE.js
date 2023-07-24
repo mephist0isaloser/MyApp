8fdd30ed24afe831cb9225d10c5a925b75211dfe
MO8 <mail4mo8@gmail.com>
2023-07-25 05:21:58 +0530

    Version 2

--- FILE.js
+++ FILE.js
@@ -1,1 +1,2 @@
 console.log("change exists")
+console.log("Version 2")
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("this is the time" + DateTime.Now);
+Console.WriterLine("version 2")
 
